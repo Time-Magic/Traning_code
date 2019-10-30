2476207c97998f8ed13fc0aace18c9c44ca832b1
Time-Magic <k199613@outlook.com>
2019-10-30 18:33:33 +0800

    新增弹性网络在波士顿上二维调参上的拟合效果，结果表明正则项将降低预测精度。 同时，预测精度对于alpha相比较L1-ratio更加敏感，但总体降低不多

--- boston_elasticnet.py
+++ boston_elasticnet.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_boston
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing.data import StandardScaler
@@ -9,18 +9,18 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator
 
-irisdata = load_iris()
-iris_X = irisdata.data
-iris_y = irisdata.target
+bostondata = load_boston()
+boston_X = bostondata.data
+boston_y = bostondata.target
 scale = StandardScaler()
-scale.fit(iris_X)
-iris_x = scale.transform(iris_X)
+scale.fit(boston_X)
+boston_x = scale.transform(boston_X)
 pca = PCA(n_components=3)
-iris_x = pca.fit_transform(iris_x)
+# boston_x = pca.fit_transform(boston_x)
 fig = plt.figure()
 ax = plt.gca(projection='3d')
-# ax.scatter(iris_x[:, 0], iris_x[:, 1], iris_x[:, 2], marker='o', c=iris_y)
-x_tran, x_test, y_tran, y_test = train_test_split(iris_x, iris_y, test_size=0.3, random_state=42)
+# ax.scatter(boston_x[:, 0], boston_x[:, 1], boston_x[:, 2], marker='o', c=boston_y)
+x_tran, x_test, y_tran, y_test = train_test_split(boston_x, boston_y, test_size=0.3, random_state=42)
 result = []
 z = np.zeros(shape=(10, 10))
 test_number = len(y_test)
@@ -28,14 +28,13 @@ for i in range(1, 11, 1):
     for j in range(1, 11, 1):
         clf = ElasticNet(alpha=i / 10, l1_ratio=j / 10).fit(x_tran, y_tran)
         y_pre = clf.predict(x_test)
-        result.append([i, j, (sum(m < 0.5 for m in abs(y_test - y_pre)) / test_number)])
-        z[i - 1, j - 1] = (sum(m < 0.5 for m in abs(y_test - y_pre)) / test_number)
+        result.append([i, j, clf.score(x_test, y_test)])
+        z[i - 1, j - 1] = clf.score(x_test, y_test)
 print(result)
 x = np.linspace(1, 10, 10)
 y = np.linspace(1, 10, 10)
 X, Y = np.meshgrid(x / 10, y / 10)
 ax.plot_surface(X, Y, z, cmap=cm.coolwarm)
-ax.set_zlim(0, 1)
 ax.zaxis.set_major_locator(LinearLocator(5))
 
 plt.show()
