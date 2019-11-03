bd8c2c3475043ab6dddddf64ca3a510afd64b6b6
Time-Magic <k199613@outlook.com>
2019-11-03 13:59:30 +0800

    新增加SVMs中SVC分类器 新增加likefunctions函数包，plot_func_region函数，给定x[:,2],flag，函数func，画出函数数据的散点图以及分类区域。

---
+++ iris_SVM.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing.data import StandardScaler
+from sklearn import svm
+from likefunctions import plot_func_region
+
+irisdata = load_iris()
+iris_X = irisdata.data
+iris_y = irisdata.target
+scale = StandardScaler()
+scale.fit(iris_X)
+iris_x = scale.transform(iris_X)
+pca = PCA(n_components=2)
+iris_x = pca.fit_transform(iris_x)
+x_tran, x_test, y_tran, y_test = train_test_split(iris_x, iris_y, test_size=0.3, random_state=42)
+clf = svm.SVC(gamma='auto').fit(x_tran, y_tran)
+y_pre = clf.predict(x_test)
+plot_func_region(x_test, y_test, lambda x: clf.predict(x))
+plt.title('SVC')
+plt.show()
+result = clf.score(x_test, y_test)
+print(result)
+# TODO:整理为函数或类形式.
+#  新增加二维分类绘图函数plot_func_region()
---
+++ likefunctions.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_func_region(x, flag, pre_func):
+    x1_min, x1_max = x[:, 0].min() - .5, x[:, 0].max() + .5
+    x2_min, x2_max = x[:, 1].min() - .5, x[:, 1].max() + .5
+    h = .01
+    xx, xy = np.meshgrid(np.arange(x1_min, x1_max, h), np.arange(x2_min, x2_max, h))
+    Z = pre_func(np.c_[xx.ravel(), xy.ravel()])
+    z = Z.reshape(xx.shape)
+    plt.contourf(xx, xy, z, cmap=plt.cm.Spectral)
+    plt.scatter(x[:, 0], x[:, 1], c=flag, cmap=plt.cm.Spectral)
