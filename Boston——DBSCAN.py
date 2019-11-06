3bfd5d6bd3942f554e7a7605d7f8857fca879690
Like <k199613@outlook.com>
2019-11-06 17:44:36 +0800

    新增加DBSCAN聚类算法，还没有调参，在调参后应该可以获得较为改善的数据集用于拟合。

---
+++ Boston——DBSCAN.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_boston
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing.data import StandardScaler
+from sklearn.cluster import DBSCAN
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+bostondata = load_boston()
+boston_X = bostondata.data
+boston_y = bostondata.target
+scale = StandardScaler()
+scale.fit(boston_X)
+boston_x = scale.transform(boston_X)
+pca = PCA(n_components=3)
+boston_full = np.c_[boston_x, boston_y]
+boston_full3 = pca.fit_transform(boston_full)
+clt = DBSCAN()
+label3 = clt.fit_predict(X=boston_full3)
+fig = plt.figure()
+ax = plt.gca(projection='3d')
+ax.scatter(boston_full3[:, 0], boston_full3[:, 1], boston_full3[:, 2], marker='o', c=label3)
+plt.show()
+'''
+x_tran, x_test, y_tran, y_test = train_test_split(boston_x, boston_y, test_size=0.3, random_state=42)
+result = []
+z = np.zeros(shape=(10, 10))
+test_number = len(y_test)
+for i in range(1, 11, 1):
+    for j in range(1, 11, 1):
+        clf = svm.SVR(C=i / 10, epsilon=j / 10,gamma='auto').fit(x_tran, y_tran)
+        y_pre = clf.predict(x_test)
+        result.append([i, j, clf.score(x_test,y_test)])
+        z[i - 1, j - 1] = clf.score(x_test,y_test)
+print(result)
+x = np.linspace(1, 10, 10)
+y = np.linspace(1, 10, 10)
+X, Y = np.meshgrid(x / 10, y / 10)
+ax.plot_surface(X, Y, z, cmap=cm.coolwarm)
+ax.set_zlim(0, 1)
+ax.zaxis.set_major_locator(LinearLocator(5))
+ax.set_xlabel('C')
+ax.set_ylabel('eplison')
+ax.set_zlabel('score')
+plt.title('Boston_SVR')
+plt.show()
+'''
+
+# TODO:整理为函数或类形式。
+#  由于SCR具有两个自由参数C及epsilon，未来需要在程序上进行两个参数方面的调参并给出相关结论
+#  波士顿房价数据集是一定需要清洗的，预测问题太差了.
