4c2c338465ca7671e9fc4f925311bd52aec2705f
Like <k199613@outlook.com>
2019-11-09 20:51:41 +0800

    新增加Digit数据的大量处理方法类，需要确定调参策略，给出调参结果。

---
+++ MNIST_HELLOWORLD/digit_prepare.py
@@ -0,0 +1,6 @@
+from asyncio import wait
+
+from MNIST_HELLOWORLD.likedigitclass import DigitAutoClassifier
+
+logicregression_digit = Digit(model='LogisticRegression', split_ratio=0.3, random_seed=1)
+wait
