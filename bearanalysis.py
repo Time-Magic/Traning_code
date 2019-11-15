587e1a9839402b0e98bab337b65f2f58fbfda749
Time-Magic <k199613@outlook.com>
2019-11-15 20:48:04 +0800

    文件的读取数据文件

--- bearanalysis.py
+++ bearanalysis.py
@@ -1,9 +1,17 @@
 from cwru import CWRU
-from scipy.fftpack import fft, ifft
-from matplotlib import pyplot as plt
 from numpy import ndarray as np
+from scipy.fftpack import fft, ifft, fftshift, fftfreq
+from matplotlib import pyplot as plt
+from scipy.io import loadmat
+from os.path import dirname
 
-bear_data = CWRU(exp='12FanEndFault', rpm='1730', length=500)
-fft_bear_data = fft(bear_data.X_train)
-plt.plot(fft_bear_data)
+a = dirname(__file__)
+f = a + '/CaseWesternReserveUniversityData/normal_2_99.mat'
+bear_dict = loadmat(f)
+data_bearing = bear_dict['X099_FE_time']
+# data_bearing=numpy.random.rand(samples)
+fft_data_bearing = fft(data_bearing)
+freq_fft_data_bearing = fftfreq(fft_data_bearing.size, 1 / 12000)
+plt.stem(freq_fft_data_bearing, (fft_data_bearing.real ** 2 + fft_data_bearing.imag ** 2)
+         , use_line_collection=True)
 plt.show()
