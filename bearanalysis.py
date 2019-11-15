4e6c0fe644a1e2a74925f07d0b558301793e114b
Like <k199613@outlook.com>
2019-11-15 22:14:21 +0800

    给出傅里叶变换并画出频谱图，但是频谱图并不对称。有些奇怪

--- bearanalysis.py
+++ bearanalysis.py
@@ -1,17 +1,20 @@
-from cwru import CWRU
-from numpy import ndarray as np
-from scipy.fftpack import fft, ifft, fftshift, fftfreq
+import numpy as np
+from scipy.fftpack import rfft, fft, ifft, fftshift, fftfreq
 from matplotlib import pyplot as plt
 from scipy.io import loadmat
 from os.path import dirname
 
+samples = 500
 a = dirname(__file__)
 f = a + '/CaseWesternReserveUniversityData/normal_2_99.mat'
 bear_dict = loadmat(f)
-data_bearing = bear_dict['X099_FE_time']
-# data_bearing=numpy.random.rand(samples)
+data_bearing = bear_dict['X099_FE_time'][:samples]
+x = np.linspace(0, 1, 1400)
+# data_bearing=7*np.sin(2*np.pi*180*x)+2.8*np.sin(2*np.pi*390*x)
+
 fft_data_bearing = fft(data_bearing)
 freq_fft_data_bearing = fftfreq(fft_data_bearing.size, 1 / 12000)
-plt.stem(freq_fft_data_bearing, (fft_data_bearing.real ** 2 + fft_data_bearing.imag ** 2)
-         , use_line_collection=True)
+freq_limit = int(samples / 2)
+signal_limit = freq_limit
+plt.plot(freq_fft_data_bearing[:freq_limit], abs(fft_data_bearing[:freq_limit]))
 plt.show()
