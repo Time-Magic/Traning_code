b60a657393189af11f99c89a73aa14e3b53f6eb0
Like <k199613@outlook.com>
2019-11-17 16:43:28 +0800

    新增加正常轴承数据集的傅里叶变换频谱图。

--- bearanalysis.py
+++ bearanalysis.py
@@ -4,17 +4,15 @@ from matplotlib import pyplot as plt
 from scipy.io import loadmat
 from os.path import dirname
 
-samples = 500
+samples = 120
+data_bearing = np.zeros((samples * 5))
 a = dirname(__file__)
 f = a + '/CaseWesternReserveUniversityData/normal_2_99.mat'
 bear_dict = loadmat(f)
-data_bearing = bear_dict['X099_FE_time'][:samples]
-x = np.linspace(0, 1, 1400)
-# data_bearing=7*np.sin(2*np.pi*180*x)+2.8*np.sin(2*np.pi*390*x)
-
+data_bearing[0:samples] = np.transpose(bear_dict['X099_FE_time'][samples * 2:samples * 3])
 fft_data_bearing = fft(data_bearing)
 freq_fft_data_bearing = fftfreq(fft_data_bearing.size, 1 / 12000)
-freq_limit = int(samples / 2)
+freq_limit = int(fft_data_bearing.size / 2)
 signal_limit = freq_limit
-plt.plot(freq_fft_data_bearing[:freq_limit], abs(fft_data_bearing[:freq_limit]))
+plt.stem(freq_fft_data_bearing[:freq_limit], abs(fft_data_bearing[:freq_limit]), linefmt='', use_line_collection=True)
 plt.show()
