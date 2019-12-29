b2f93346f4ef0fb887cf86732441abed3c948065
Like <k199613@outlook.com>
2019-12-29 15:22:25 +0800

    更新至ANN调整，但多层感知机模型相比之下更复杂，主要是数据维数与数据量之间 的矛盾，数据量较小时为了保证一定的模型深度，需要数据特征较少，需要特征提取。

--- likefunctions_boston.py
+++ likefunctions_boston.py
@@ -1,13 +1,24 @@
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.decomposition import PCA
-def spl_and_Std_boston(X_boston='Xboston', Y_boston='Yboston'):
+from sklearn.datasets import load_boston
+from xgboost import XGBRegressor
+from sklearn.svm import SVR
+from sklearn.neural_network import MLPRegressor
+from math import ceil
+
+
+def spl_and_Std_boston(X_boston='Xboston', Y_boston='Yboston', origin=False, factual=0.33):
     X_boston = np.loadtxt('Boston_data', delimiter=',')
     Y_boston = np.loadtxt('Boston_txt', delimiter=',')
-    X_train, X_test, y_train, y_test = train_test_split(X_boston, Y_boston, test_size=0.33, random_state=42)
+    if origin == True:
+        X_boston = load_boston().data
+        Y_boston = load_boston().target
+    X_train, X_test, y_train, y_test = train_test_split(X_boston, Y_boston, test_size=1 - factual, random_state=42)
     scl = StandardScaler().fit(X_train)
     x_train = scl.transform(X_train)
     x_test = scl.transform(X_test)
@@ -78,6 +89,86 @@ def Dtr_pca(dimension=5):
 def Dtr_lossf(function='mse'):
     x_train, x_test, y_train, y_test = spl_and_Std_boston()
     clf = DecisionTreeRegressor(max_depth=5)
+    clf.fit(x_train, y_train)
+    result = np.array(clf.score(x_test, y_test))
+    result_train = np.array(clf.score(x_train, y_train))
+    return result, result_train
+
+
+def RandomForestRegression_number(number=100):
+    x_train, x_test, y_train, y_test = spl_and_Std_boston()
+    clf = RandomForestRegressor(n_estimators=number)
+    clf.fit(x_train, y_train)
+    result = np.array(clf.score(x_test, y_test))
+    result_train = np.array(clf.score(x_train, y_train))
+    return result, result_train
+
+
+def xgboost_number(number=100):
+    x_train, x_test, y_train, y_test = spl_and_Std_boston()
+    clf = XGBRegressor(n_estimators=number, objective='reg:squarederror')
+    clf.fit(x_train, y_train)
+    result = np.array(clf.score(x_test, y_test))
+    result_train = np.array(clf.score(x_train, y_train))
+    return result, result_train
+
+
+def xgboost_depth(depth=100):
+    x_train, x_test, y_train, y_test = spl_and_Std_boston()
+    clf = XGBRegressor(n_estimators=100, max_depth=depth, objective='reg:squarederror')
+    clf.fit(x_train, y_train)
+    result = np.array(clf.score(x_test, y_test))
+    result_train = np.array(clf.score(x_train, y_train))
+    return result, result_train
+
+
+def svr_epsilon(epsilon=0.1):
+    x_train, x_test, y_train, y_test = spl_and_Std_boston()
+    clf = SVR(epsilon=epsilon)
+    clf.fit(x_train, y_train)
+    result = np.array(clf.score(x_test, y_test))
+    result_train = np.array(clf.score(x_train, y_train))
+    return result, result_train
+
+
+def svr_C(C=1):
+    x_train, x_test, y_train, y_test = spl_and_Std_boston()
+    clf = SVR(epsilon=0.7, C=C)
+    clf.fit(x_train, y_train)
+    result = np.array(clf.score(x_test, y_test))
+    result_train = np.array(clf.score(x_train, y_train))
+    return result, result_train
+
+
+def svr_kernel(kernel='rbf'):
+    x_train, x_test, y_train, y_test = spl_and_Std_boston()
+    clf = SVR(epsilon=0.7, C=15, kernel=kernel)
+    clf.fit(x_train, y_train)
+    result = np.array(clf.score(x_test, y_test))
+    result_train = np.array(clf.score(x_train, y_train))
+    return result, result_train
+
+
+def ann_depth(depth=3, init=5):
+    x_train, x_test, y_train, y_test = spl_and_Std_boston()
+    layer = np.array([ceil(init * 1.2 ** i) for i in range(1, depth + 1)])
+    pca = PCA(n_components=2)
+    pca.fit_transform(x_train)
+    pca.transform(x_test)
+    clf = MLPRegressor(hidden_layer_sizes=layer, max_iter=100000, solver='lbfgs')
+    clf.fit(x_train, y_train)
+    result = np.array(clf.score(x_test, y_test))
+    result_train = np.array(clf.score(x_train, y_train))
+    return result, result_train
+
+
+def ann_L2(depth=3, init=5, L2=0.001):
+    x_train, x_test, y_train, y_test = spl_and_Std_boston()
+    layer = np.array([ceil(init * 0.9 ** i) for i in range(1, depth + 1)])
+    pca = PCA(n_components=13)
+    pca.fit_transform(x_train)
+    pca.transform(x_test)
+    clf = MLPRegressor(hidden_layer_sizes=layer, max_iter=100000, solver='lbfgs', alpha=L2)
     clf.fit(x_train, y_train)
     result = np.array(clf.score(x_test, y_test))
     result_train = np.array(clf.score(x_train, y_train))
