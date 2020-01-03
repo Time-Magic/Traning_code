9013bdba06b9070b44caeadc8f225e0506364ebc
Like <k199613@outlook.com>
2020-01-03 17:12:59 +0800

    删除了一些无用的功能组件

--- MNIST_HELLOWORLD/likedigitclass.py
+++ MNIST_HELLOWORLD/likedigitclass.py
@@ -45,52 +45,6 @@ class DigitEnsembleClassifier():
         self.clf.fit(self.x, self.y)
         self.result = self.clf.cv_results_
 
-    def linedata(self, xlabel, const_param1='C', paramvalue1=1,
-                 const_param2='solver', paramvalue2='saga', target='mean_test_score'):
-        '''按照指定要求绘制线图图像
-        :parameter
-
-        xlabel:str
-        可调自变量
-
-        const_param1:str
-        绘图需要固定的可调参数1
-
-        paramvalue1:float/int/str
-        可调参数1的固定值
-
-         const_param2:str
-        绘图需要固定的可调参数2
-
-        paramvalue2:float/int/str
-        可调参数2的固定值
-
-        target:str
-        目标参数值，将作为纵坐标数值
-
-        :returns
-
-        linex:float
-        自变量参数值
-
-        liney:float
-        因变量参数值
-
-        condition_label:str
-        条件标签
-        '''
-        result = self.result
-        const_param11 = 'param_' + const_param1
-        const_param22 = 'param_' + const_param2
-        param = 'param_' + xlabel
-        idx1 = result[const_param11] == paramvalue1
-        idx2 = result[const_param22] == paramvalue2
-        idx = ~(~idx1 + ~idx2)
-        linex = result[param][idx]
-        liney = result[target][idx]
-        condition_label = const_param1.capitalize() + '=' + paramvalue1 + '\n' + const_param2.capitalize() + '=' + paramvalue2 + '\n' + param.upper()
-        return linex, liney, condition_label
-
     def export(self, name='data'):
         '''输出数据图表,传入参数无文件类型名
         :parameter
